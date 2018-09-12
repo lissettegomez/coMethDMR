d9232ae5196c7b45ce42082980b85eecf9ed1e19
Gomez <lxg255@miami.edu>
2018-09-12 16:35:25 -0400

    Update function MarkComethylatedCpGs issue #3

--- R/MarkComethylatedCpGs.R
+++ R/MarkComethylatedCpGs.R
@@ -3,7 +3,8 @@
 
 #' Find contiguous comethylated regions
 #'
-#' @param betaCluster_mat matrix of beta values
+#' @param betaCluster_mat matrix of beta values, with rownames = sample ids, column names = CpG ids
+#' and ordered by genomic position
 #' @param rDropThresh_num min correlation between a cpg in the region with the rest of the CpGs
 #'
 #' @importFrom psych alpha
@@ -12,31 +13,36 @@
 #' @export
 #'
 #' @examples
-#'
+#' attach("tests/betaCluster_mat_example1.RData")
+#' MarkComethylatedCpGs(betaCluster_mat = betaMatrix_ex1)
+#' attach("tests/betaCluster_mat_example2.RData")
+#' MarkComethylatedCpGs(betaCluster_mat = betaMatrix_ex2)
+#' attach("tests/betaCluster_mat_example3.RData")
+#' MarkComethylatedCpGs(betaCluster_mat = betaMatrix_ex3)
 
-
-# library(psych)
-# library(bumphunter)
 
 MarkComethylatedCpGs <- function (betaCluster_mat, rDropThresh_num=0.5) {
 
 
-  ### Calculate alpha ###
+  ### Calculate alpha and Store CpGs ###
   clusterAlpha_ls <- alpha(betaCluster_mat, warnings = FALSE)
+  CpGs_char <- rownames(clusterAlpha_ls$alpha.drop)
 
   ### Drop CpGs with r.drop < threshold_r_num ###
-  dropCpGs_char <- row.names(subset(clusterAlpha_ls$item.stats,
-                              clusterAlpha_ls$item.stats$r.drop < rDropThresh_num))  ###drop these cpgs
+  # drop these cpgs
+  dropCpGs_char <- row.names(
+    subset(clusterAlpha_ls$item.stats,
+           clusterAlpha_ls$item.stats$r.drop < rDropThresh_num)
+  )
 
-  CpGs_df <- as.data.frame(rownames(clusterAlpha_ls$alpha.drop))
-
-  colnames(CpGs_df) <- "cpg"
-
-  CpGs_df$alpha <- ifelse(
-    row.names(clusterAlpha_ls$alpha.drop) %in% dropCpGs_char, 0, 1)   ##(drop=0, keep=1)
-
-  CpGs_df$ind <- 1:ncol(betaCluster_mat)
+  ###  Create Output Data Frame  ###
+  CpGs_df <- data.frame(
+    CpG = CpGs_char,
+    alpha = ifelse(CpGs_char %in% dropCpGs_char, 0, 1), ##(drop=0, keep=1)
+    ind = 1:ncol(betaCluster_mat)
+  )
 
   CpGs_df
+
 
 }
