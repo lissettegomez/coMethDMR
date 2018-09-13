044e05f94ebf6e6a70bdec6384dae2fadec116f8
Gomez <lxg255@miami.edu>
2018-09-13 15:56:19 -0400

    Function FindComethylatedRegions updated Issue #3

--- R/FindComethylatedRegions.R
+++ R/FindComethylatedRegions.R
@@ -1,86 +1,59 @@
 
 
-
 #' Find contiguous comethylated regions
 #'
-#' @param betaCluster_mat matrix of beta values
+#' @param CpGs_df a dataframe with cpg name (CpG), keep=1/drop=0 (alpha), ind=1:number of CpGs in the region
 #' @param minCpGs_int n integer, minimum nubmer of cpgs for resulting regions
-#' @param CpGlocations_df a dataframe with cpg name (cpg), chromosome (CHR) and location (MAPINFO), row.names = cpg
-#' @param rDropThresh_num min correlation between a cpg in the region with the rest of the CpGs
 #'
-#' @importFrom bumphunter clusterMaker
-#'
-#' @return
+#' @return data frame with CpG and subregion number
 #' @export
 #'
 #' @examples
+#' attach("tests/betaCluster_mat_example1.RData")
+#' CpGs_df <- MarkComethylatedCpGs(betaCluster_mat = betaMatrix_ex1)
+#' FindComethylatedRegions2(CpGs_df)
 #'
-
-
-# library(psych)
-# library(bumphunter)
-
-FindComethylatedRegions <- function (betaCluster_mat,
-                                  minCpGs_int=3,
-                                  CpGlocations_df,
-                                  rDropThresh_num=0.5) {
-
-  ### Calculate alpha ###
-  clusterAlpha_ls <- alpha(betaCluster_mat, warnings = FALSE)
-
-  ### Drop CpGs with r.drop < threshold_r_num ###
-  dropCpGs_char <- row.names(subset(clusterAlpha_ls$item.stats,
-                              clusterAlpha_ls$item.stats$r.drop < rDropThresh_num))  ###drop these cpgs
-
-  CpGs_df <- as.data.frame(rownames(clusterAlpha_ls$alpha.drop))
-
-  colnames(CpGs_df) <- "cpg"
-
-  CpGs_df$alpha <- ifelse(
-    row.names(clusterAlpha_ls$alpha.drop) %in% dropCpGs_char, 0, 1)   ##(drop=0, keep=1)
-
-  CpGs_df$ind <- 1:ncol(betaCluster_mat)
+FindComethylatedRegions2 <- function(CpGs_df, minCpGs_int=3){
 
   ### Get contiguous regions of CpGs ###
-  contiguousRegions <- getSegments(CpGs_df$alpha, cutoff = 1)
 
+  contiguousRegion_ls <- getSegments(CpGs_df$alpha, cutoff = 1)
+  nSegs_int <- length(contiguousRegion_ls$upIndex)
 
-  contiguous_idx <- data.frame(matrix(ncol = 1, nrow = 0))
+  if (nSegs_int > 0){
 
-  if (length(contiguousRegions$upIndex) > 0){
+    ### Select segments with number of CpGs >= minCpGs ###
 
-    for (j in 1:length(contiguousRegions$upIndex))
+    contiguous_int <- lengths(contiguousRegion_ls$upIndex)
+    contiguousMinCpGs_idx <- which(contiguous_int >= minCpGs_int)
+    nSegsMinCpGs_int <- length(contiguousMinCpGs_idx)
 
-    {contiguous_idx <- rbind(contiguous_idx,
-                             length(contiguousRegions$upIndex[[j]]))}
+    ### Create output dataframe with CpGs and contiguous comethylated subregion number ###
 
-    contiguosMinCpGs_idx <- as.numeric(
-      rownames(subset(contiguous_idx, contiguous_idx[,1] >= minCpGs_int)))
+      if (nSegsMinCpGs_int > 0){
 
-    if (length(contiguosMinCpGs_idx) > 0){
+        contiguousRegionsCpGs<-data.frame(matrix(ncol=2,nrow=0))
 
-      contiguousRegionsCpGs<-data.frame(matrix(ncol=2,nrow=0))
+        for (u in 1:nSegsMinCpGs_int)
 
-      for (u in 1:length(contiguosMinCpGs_idx))
+          {contiguousRegionsCpGs <- rbind(
+            contiguousRegionsCpGs,
+            cbind(
+              as.data.frame(
+                subset(CpGs_df,ind %in% contiguousRegion_ls$upIndex[[contiguousMinCpGs_idx[u]]], select="CpG")),
+              rep(u, length(contiguousRegion_ls$upIndex[[contiguousMinCpGs_idx[u]]]))))}
 
-      {contiguousRegionsCpGs <- rbind(
-        contiguousRegionsCpGs,
-        cbind(
-          as.data.frame(
-          subset(CpGs_df,ind %in% contiguousRegions$upIndex[[contiguosMinCpGs_idx[u]]], select="cpg")),
-              rep(u, length(contiguousRegions$upIndex[[contiguosMinCpGs_idx[u]]]))))}
-
-    } else {
-      contiguousRegionsCpGs<-cbind(as.data.frame(CpGs_df$cpg),
-                                   rep(0,length(CpGs_df$cpg)))}
+      } else {
+        contiguousRegionsCpGs<-cbind(as.data.frame(CpGs_df$CpG),
+                                     rep(0,length(CpGs_df$CpG)))}
 
   } else {
-    contiguousRegionsCpGs<-cbind(as.data.frame(CpGs_df$cpg),
-                                 rep(0,length(CpGs_df$cpg)))}
+    contiguousRegionsCpGs<-cbind(as.data.frame(CpGs_df$CpG),
+                                 rep(0,length(CpGs_df$CpG)))}
 
-  colnames(contiguousRegionsCpGs)<-c("ProbeID","subisland")
+  colnames(contiguousRegionsCpGs)<-c("ProbeID","Subregion")
 
-
+  contiguousRegionsCpGs
 
 
 }
