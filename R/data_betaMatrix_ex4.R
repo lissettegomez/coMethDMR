801d05089e1f0cbece69648b412475494ac0d4e4
Gomez <lxg255@miami.edu>
2018-10-10 09:27:51 -0400

    documentation for ex4, issue #10

---
+++ R/data_betaMatrix_ex4.R
@@ -0,0 +1,11 @@
+#'  Alzheimer's Prefrontal Cortex (PFC) Methylation Data
+#'
+#' @description Subset of an Alzheimer's methylation data set,
+#'   with beta values for CpGs.
+#'
+#' @format A subset of a data frame containing beta values for 52 CpGs in
+#'   one CpG island in 110 subjects.
+#'
+#'
+#' @source GEO accession: GSE59685
+"betaMatrix_ex4"
