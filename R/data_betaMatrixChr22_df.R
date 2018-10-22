8289ffa5684d310c70201e37e5f56c9cc5cedbd2
Gomez <lxg255@miami.edu>
2018-10-22 11:35:34 -0400

    file for documentation of the example dataset for beta values was added, Issue #18

---
+++ R/data_betaMatrixChr22_df.R
@@ -0,0 +1,11 @@
+#'  Alzheimer's Prefrontal Cortex (PFC) Methylation Data
+#'
+#' @description Subset of an Alzheimer's methylation dataset,
+#'   with beta values for CpGs.
+#'
+#' @format A subset of a data frame containing beta values for 8552 CpGs in
+#'   Chr22 in 20 subjects.
+#'
+#'
+#' @source GEO accession: GSE59685
+"betaMatrixChr22_df"
