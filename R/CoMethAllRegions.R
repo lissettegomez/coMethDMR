9f2756e53ee052f6fa4f2d741aa2a93be1708f4a
Gomez <lxg255@miami.edu>
2018-11-12 16:59:37 -0500

    function 'CoMethAllRegions' was updated, issue #24

--- R/CoMethAllRegions.R
+++ R/CoMethAllRegions.R
@@ -27,6 +27,18 @@
 #' @export
 #'
 #' @examples
+#'    data(betaMatrixChr22_df)
+#'    CoMethAllRegions(
+#'       betaMatrix = betaMatrixChr22_df,
+#'       file = system.file(
+#'          "extdata",
+#'          "CpGislandsChr22_ex.RDS",
+#'          package = 'coMethDMR',
+#'          mustWork = TRUE),
+#'       fileType = "RDS",
+#'       arrayType = "450k",
+#'       returnAllCpGs = FALSE
+#'       )
 #'
 CoMethAllRegions <- function(betaMatrix,
                              file, fileType = c("gmt","RDS"),
