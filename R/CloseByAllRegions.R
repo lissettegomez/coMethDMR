ace11fd1acea55cb8269e7a6111da71cd7c47b31
Gomez <lxg255@miami.edu>
2018-11-12 11:22:43 -0500

    the function 'CloseByAllRegions' and its example were updated, issue #20

--- R/CloseByAllRegions.R
+++ R/CloseByAllRegions.R
@@ -10,7 +10,7 @@
 #' @param fileType the output files can be saved as .gmt or .RDS,
 #'    .gmt files can be open as flat text file, .RDS files are 50% the size of
 #'    .gmt files, but cannot be open
-#' @param dataDir link to the directory where the filtered files will be saved
+#' @param file file where the close by regions will be saved
 #' @param ...
 #'
 #' @return a file with the close by regions
@@ -22,12 +22,17 @@
 #' @examples
 #'    CloseByAllRegions(
 #'      genomicRegionType = "ISLAND", arrayType = "450k", maxGap = 50,
-#'      minCpGs = 3, fileType = "gmt", dataDir = "inst"
+#'      minCpGs = 3, fileType = "gmt", file = "closeByRegions"
 #'    )
 #'
 CloseByAllRegions <- function(
-  genomicRegionType = c("ISLAND", "TSS1500", "TSS200", "UTR5", "EXON1", "GENEBODY", "UTR3", "NSHORE", "SSHORE", "NSHELF", "SSHELF"),
-  arrayType = c("450k","EPIC"), maxGap = 200, minCpGs = 3, fileType = c("gmt","RDS"), dataDir, ...
+  genomicRegionType = c("ISLAND", "TSS1500", "TSS200", "UTR5", "EXON1",
+                        "GENEBODY", "UTR3", "NSHORE", "SSHORE", "NSHELF", "SSHELF"),
+  arrayType = c("450k","EPIC"),
+  maxGap = 200,
+  minCpGs = 3,
+  fileType = c("gmt","RDS"),
+  file, ...
   ){
 
   if(maxGap == 200 & minCpGs == 3){
@@ -40,7 +45,11 @@ CloseByAllRegions <- function(
     arrayType <- match.arg(arrayType)
 
     ### Read gmtFile ###
-    gmtFile <- read_gmt(paste("inst/extdata/", genomicRegionType, "Ind.gmt", sep = ""))
+    fileName <- paste(genomicRegionType, "Ind.gmt", sep = "")
+    data_path <- system.file(
+      "extdata",fileName, package = 'coMethDMR', mustWork = TRUE
+    )
+    gmtFile <- read_gmt(data_path)
 
     ### Extract regions with three or more CpGs ###
     gmtFile3CpGs_ls <- gmtFile$pathway [lapply(gmtFile$pathway, length) >= 3]
@@ -52,29 +61,35 @@ CloseByAllRegions <- function(
     )
 
     ### Remove 'NULL' elements of the list ###
-    closeByRegionsNoNull_ls <- unlist(closeByRegions_ls, recursive = F)
+    closeByRegionsNoNull_ls <- unlist(closeByRegions_ls, recursive = FALSE)
 
     ### Order CpGs in each cluster by location to name the cluster ###
     ### 8.202557 secs for 162 regions, after unlisting 1000 regions from previous step
     closeByRegionsOrderedDF_ls <- lapply(
-      closeByRegionsNoNull_ls, OrderCpGsByLocation, arrayType, output = "dataframe"
+      closeByRegionsNoNull_ls,
+      FUN = OrderCpGsByLocation,
+      arrayType,
+      output = "dataframe"
     )
 
     ### Name each cluster with genomic region ###
-    closeByRegionsNames_ls <- lapply(closeByRegionsOrderedDF_ls, NameRegion)
+    closeByRegionsNames_ls <- lapply(
+      closeByRegionsOrderedDF_ls, FUN = NameRegion
+    )
 
     ### Order CpGs in each cluster by location ###
-    closeByRegionsOrdered_ls <- lapply(closeByRegionsOrderedDF_ls, function(x) x[,"cpg"])
+    closeByRegionsOrdered_ls <- lapply(
+      closeByRegionsOrderedDF_ls, function(x) x[,"cpg"])
 
     ### Create gmt file ###
     out_CloseByRegions <- CreatePathwayCollection(
-      pathways = closeByRegionsOrdered_ls, TERMS = unlist(closeByRegionsNames_ls)
+      pathways = closeByRegionsOrdered_ls,
+      TERMS = unlist(closeByRegionsNames_ls)
     )
 
     ### Select and return output ###
     fileType <- match.arg(fileType)
-    path_char <- paste(dataDir, genomicRegionType, sep = "/")
-    fileName <- paste(path_char, fileType, sep = ".")
+    fileName <- paste(file, fileType, sep = ".")
     if (fileType == "RDS") {
       saveRDS(out_CloseByRegions, file = fileName)
     } else {
