275ba33771365cfd357504b55d029d3c6b1dff5f
Gomez <lxg255@miami.edu>
2018-11-15 16:32:56 -0500

    functions 'lmmTest' and '.MakeLmmFormula' were updated, issues #25 and #26

---
+++ R/MakeLmmFormula.R
@@ -0,0 +1,32 @@
+
+.MakeLmmFormula <- function(contPheno_char, covariates_char = NULL,
+                            modelType = c("randCoeffMixed", "mixed")){
+
+  modelType <- match.arg(modelType)
+  baseMod_char <- "Mvalue ~ (1|Sample)"
+  randomCoef_char <- paste0("(",contPheno_char, "|ProbeID)")
+  cov_char <- paste(covariates_char, collapse = " + ")
+
+
+  ###    ###
+  if(modelType == "randCoeffMixed"){
+
+    ifelse(
+      is.null(covariates_char),
+      rcMod_char <- paste(baseMod_char, randomCoef_char, contPheno_char, sep = " + "),
+      rcMod_char <- paste(baseMod_char, randomCoef_char, contPheno_char, cov_char, sep = " + ")
+    )
+
+
+  } else {
+
+    ifelse(
+      is.null(covariates_char),
+      rcMod_char <- paste(baseMod_char, contPheno_char, sep = " + "),
+      rcMod_char <- paste(baseMod_char, contPheno_char, cov_char, sep = " + ")
+    )
+
+  }
+
+
+}
--- R/lmmTest.R
+++ R/lmmTest.R
@@ -3,8 +3,11 @@
 #'
 #' @param betaMatrix matrix of beta values for one contiguous comethylated region,
 #'    with row names = CpG ids, column names = sample ids
-#' @param pheno a data frame with phenotype and covariates
-#' @param model model used to fit mixed model
+#' @param pheno_df a data frame with phenotype and covariates
+#'    (sample ID column = "Sample")
+#' @param contPheno_char character string of the phenotype name
+#' @param covariates_char character vector of covariate names
+#' @param modelType model used to fit mixed model
 #'
 #' @return list of pvalue and median correlation
 #'    for the contiguous comethylated region being tested
@@ -13,7 +16,17 @@
 #' @importFrom lmerTest lmer
 #'
 #' @examples
-lmmTest <- function(betaMatrix, pheno, method = c("randomCoefficientMixedModel", "mixedModelWithInterceptOnly"))  {
+#'   data(betaMatrixChr22_df)
+#'   CpGsChr22_char<-c("cg02953382", "cg12419862", "cg24565820", "cg04234412",
+#'       "cg04824771", "cg09033563", "cg10150615", "cg18538332", "cg20007245",
+#'       "cg23131131", "cg25703541")
+#'   coMethCpGs <- CoMethSingleRegion(CpGsChr22_char, betaMatrixChr22_df)
+#'   coMethBetaMatrix <- betaMatrixChr22_df[coMethCpGs$CpGsSubregions[[1]], ]
+#'   data(pheno_df)
+#'   lmmTest(betaMatrix = coMethBetaMatrix, pheno_df, contPheno_char = "stage",
+#'       covariates_char = c("age.brain", "sex"), modelType = "randCoeffMixed")
+lmmTest <- function(betaMatrix, pheno_df, contPheno_char, covariates_char,
+                    modelType = c("randCoeffMixed", "mixed"))  {
 
   ### Transpose betaMatrix from wide to long ###
   betaMatrix$ProbeID <- row.names(betaMatrix)
@@ -26,35 +39,27 @@ lmmTest <- function(betaMatrix, pheno, method = c("randomCoefficientMixedModel",
     timevar = "Sample")
 
   ### Calculate M values ###
-  betaMatrixTransp_df$Mvalue <- log2(betaMatrixTransp_df$beta/(1-betaMatrixTransp_df$beta))
+  betaMatrixTransp_df$Mvalue <- log2(
+    betaMatrixTransp_df$beta/(1-betaMatrixTransp_df$beta)
+  )
 
   ### Merge transposed beta matrix with phenotype ###
-  betaMatrixPheno_df <- merge(betaMatrixTransp_df, pheno, by="Sample")
+  betaMatrixPheno_df <- merge(betaMatrixTransp_df, pheno_df, by="Sample")
 
 
   ### Run the mixed model ###
-  if (model == "randomCoefficientMixedModel"){
 
-    tryCatch({
-      f<-lmer(Mvalue ~stage + (stage|ProbeID) +(1|Sample) + age.brain + sex + as.factor(Mplate), betaMatrixPheno_df)
-      ps<-coef(summary(f))[2,5]
-    }, error=function(e){})
+  modelFormula_char <- .MakeLmmFormula(contPheno_char, covariates_char, modelType)
+  tryCatch({
+    f <- lmer(as.formula(modelFormula_char), betaMatrixPheno_df)
+    ps <- coef(summary(f))[2,5]
+  }, error=function(e){})
 
-
-  } else {
-
-
-    tryCatch({
-      f<-lmer(Mvalue ~stage + (1|Sample) + age.brain + sex + as.factor(Mplate), betaMatrixPheno_df)
-      ps<-coef(summary(f))[2,5]
-    }, error=function(e){})
-
-  }
 
   ### Return results ###
-  medianCorr<-median(cor(t(betaMatrix[-ncol(betaMatrix)])))
-  results<-list(ps,medianCorr)
-  names(results)<-c("Pval_Mixed_Model","Median_Corr")
+  medianCorr <- median(cor(t(betaMatrix[-ncol(betaMatrix)])))
+  results <- list(ps,medianCorr)
+  names(results) <- c("Pval_Mixed_Model","Median_Corr")
   results
 
 
