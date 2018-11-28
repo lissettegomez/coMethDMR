7b116fd4909e176e3e266d4b1f565834b719e01a
Lily Wang <lily.wangg@gmail.com>
2018-11-28 09:49:06 -0500

    documentation update

--- R/CoMethSingleRegion.R
+++ R/CoMethSingleRegion.R
@@ -1,15 +1,19 @@
 #
-#' Wrapper Function to find contiguous and comethyalted sub-regions within a pre-defined genomic region
+#' Wrapper function to find contiguous and comethyalted sub-regions within a
+#' pre-defined genomic region
 #'
 #' @param CpGs_char vector of CpGs in the inputting pre-defined genomic region.
-#' @param betaMatrix matrix of beta values, with row names = CpG ids,
-#'    column names = sample ids
-#' @param arrayType Type of array, can be "450k" or "EPIC"
-#' @param returnAllCpGs when there is not a contiguous comethylated region
 #'
-#' indicates if outputting all the CpGs in the region
-#'     or
-#'    only the CpGs in the contiguous comethylated regions
+#' @param betaMatrix matrix of beta values, with row names = CpG ids,
+#'    column names = sample ids. This should include the CpGs in \code{CpGs_char}, as well as
+#'    additional CpGs.
+#'
+#' @param arrayType Type of array, can be "450k" or "EPIC"
+#'
+#' @param returnAllCpGs When there is not a contiguous comethylated region in the inputing
+#' pre-defined region, \code{returnAllCpGs = 1} indicates outputting all the CpGs in the input
+#' region, \code{returnAllCpGs = 0} indicates not returning any CpG.
+#'
 #' @return A list with two components:
 #'
 #' \itemize{
@@ -26,10 +30,13 @@
 #'
 #' @examples
 #'    data(betaMatrixChr22_df)
-#'    CpGsChr22_char<-c("cg02953382", "cg12419862", "cg24565820", "cg04234412",
+#'
+#'    CpGsChr22_char <- c("cg02953382", "cg12419862", "cg24565820", "cg04234412",
 #'       "cg04824771", "cg09033563", "cg10150615", "cg18538332", "cg20007245",
 #'       "cg23131131", "cg25703541")
-#'    CoMethSingleRegion(CpGsChr22_char, betaMatrixChr22_df)
+#'
+#'    CoMethSingleRegion (CpGsChr22_char, betaMatrixChr22_df)
+#'
 CoMethSingleRegion <- function(CpGs_char, betaMatrix, rDropThresh_num = 0.5,
                                arrayType = c("450k","EPIC"),
                                returnAllCpGs = TRUE){
