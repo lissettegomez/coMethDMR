9e9e6055f541dc9600d4993908ea88c2b225735c
Gomez <lxg255@miami.edu>
2018-11-28 09:37:22 -0500

    lmmTest and lmmTestAllRegions updates, issue #25 and #31

--- R/lmmTestAllRegions.R
+++ R/lmmTestAllRegions.R
@@ -30,15 +30,15 @@
 #'
 #'    lmmTestAllRegions(
 #'      betaMatrixAllRegions = betaMatrixChr22_df,
-#'      inFile,
-#'      inFileType = "RDS",
-#'      arrayType = "450k",
-#'      returnAllCpGs = FALSE,
 #'      pheno_df,
 #'      contPheno_char = "stage",
 #'      covariates_char = c("age.brain", "sex"),
-#'      modelType = "randCoeffMixed",
-#'      outFile = "outEx.txt"
+#'      inFile,
+#'      outFile = "outEx.txt",
+#'      inFileType = "RDS",
+#'      arrayType = "450k",
+#'      returnAllCpGs = FALSE,
+#'      modelType = "randCoeffMixed"
 #'    )
 #'
 lmmTestAllRegions <- function(betaMatrixAllRegions, pheno_df,
@@ -65,7 +65,7 @@ lmmTestAllRegions <- function(betaMatrixAllRegions, pheno_df,
   results_ls <- lapply(
     coMethBetaMatrix_ls,
     FUN = lmmTest,
-    pheno_df, contPheno_char, covariates_char, modelType
+    pheno_df, contPheno_char, covariates_char, modelType, arrayType
   )
 
   ### Output results ###
@@ -75,14 +75,28 @@ lmmTestAllRegions <- function(betaMatrixAllRegions, pheno_df,
       lapply(results_ls, function(x) x$`Pval_Mixed_Model`)
     )),
     Pval_Mixed_Model2 = unname(
-      sapply(results_ls, function(x) x$`Pval_Mixed_Model`)
-    ),
-    Median_Corr = unlist(unname(
-      lapply(results_ls, function(x) x$`Median_Corr`)))
+      sapply(results_ls, function(x) x$`model`)
+    )
   )
 
   write.csv(out, outFile, quote = FALSE, row.names = FALSE)
 
 }
 
+# $`chr22:18268062-18268249`
+# $`chr22:18268062-18268249`$`model`
+#               Region_Name    Estimate Std..Error       df t.value   Pr...t..
+# 1 chr22:18268062-18268249 -0.07256779  0.0403895 14.72166 -1.7967 0.09292632
+#
+# $`chr22:18268062-18268249`$CpGs
+# [1] "cg12460175" "cg14086922" "cg21463605"
+#
+#
+# $`chr22:18531243-18531447`
+# $`chr22:18531243-18531447`$`model`
+#               Region_Name    Estimate Std..Error      df  t.value  Pr...t..
+# 1 chr22:18531243-18531447 -0.07127557   0.047668 16.4677 -1.49525 0.1537658
+#
+# $`chr22:18531243-18531447`$CpGs
+# [1] "cg06961233" "cg08819022" "cg25257671"
 
