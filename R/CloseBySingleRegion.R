01288d4cb86479908f8a2535f3c8e4b9773c4077
gabrielodom <gabriel_odom@baylor.edu>
2018-12-05 12:32:35 -0500

    Fixed spacing and indentation

--- R/CloseBySingleRegion.R
+++ R/CloseBySingleRegion.R
@@ -1,31 +1,43 @@
-
 #' Extract clusters of CpGs located closely in a genomic region.
 #'
 #' @param CpGs_char a list of CpG IDs
 #' @param arrayType Type of array, 450k or EPIC
 #' @param maxGap an integer, genomic locations within maxGap from each other
 #'    are placed into the same cluster
-#' @param minCpGs an integer, minimum number of CpGs for the resulting CpG cluster
+#' @param minCpGs an integer, minimum number of CpGs for the resulting CpG
+#'    cluster
 #'
-#' @return a list, each item in the list is a character vector of CpG IDs located closely (i.e. in the same cluster)
+#' @return a list, each item in the list is a character vector of CpG IDs
+#'    located closely (i.e. in the same cluster)
 #'
-#' @details Note that this function depends only on CpG locations, and not on any methylation data. The algorithm is
-#'  based on the \code{clusterMaker} function in \code{bumphunter} R package. Each cluster is essentially a group of
-#'  CpG locations such that two consecutive locations in the clsuter are separated by less than
-#'  \code{maxGap}.
+#' @details Note that this function depends only on CpG locations, and not on
+#'    any methylation data. The algorithm is based on the
+#'    \code{\link[bumphunter]{clusterMaker}} function in the \code{bumphunter}
+#'    R package. Each cluster is essentially a group of CpG locations such that
+#'    two consecutive locations in the clsuter are separated by less than
+#'    \code{maxGap}.
 #'
 #' @importFrom bumphunter clusterMaker
 #'
 #' @export
 #'
 #' @examples
-#'    CpGs_char <- c("cg02505293", "cg03618257", "cg04421269", "cg17885402",
-#'        "cg19890033", "cg20566587", "cg27505880")
-#'    cluster_ls <- CloseBySingleRegion(CpGs_char, arrayType="450k", maxGap=100, minCpGs=3)
+#'    CpGs_char <- c(
+#'      "cg02505293", "cg03618257", "cg04421269", "cg17885402", "cg19890033",
+#'      "cg20566587", "cg27505880"
+#'    )
 #'
-CloseBySingleRegion <- function(CpGs_char, arrayType = c("450k","EPIC"), maxGap = 200, minCpGs = 3){
+#'    cluster_ls <- CloseBySingleRegion(
+#'      CpGs_char, arrayType = "450k", maxGap = 100, minCpGs = 3
+#'    )
+#'
+CloseBySingleRegion <- function(CpGs_char,
+                                arrayType = c("450k","EPIC"),
+                                maxGap = 200, minCpGs = 3){
 
-  CpGsOrdered_df <- OrderCpGsByLocation(CpGs_char, arrayType, output = "dataframe")
+  CpGsOrdered_df <- OrderCpGsByLocation(
+    CpGs_char, arrayType, output = "dataframe"
+  )
 
   ### Find close by clusters ###
   chr <- CpGsOrdered_df$chr
--- R/CoMethSingleRegion.R
+++ R/CoMethSingleRegion.R
@@ -1,50 +1,56 @@
-#
 #' Wrapper function to find contiguous and comethyalted sub-regions within a
-#' pre-defined genomic region
+#'   pre-defined genomic region
 #'
 #' @param CpGs_char vector of CpGs in the inputting pre-defined genomic region.
-#'
 #' @param betaMatrix matrix of beta values, with row names = CpG ids,
-#'    column names = sample ids. This should include the CpGs in \code{CpGs_char}, as well as
-#'    additional CpGs.
-#'
-#' @param rDropThresh_num thershold for min correlation between a cpg with sum of the
-#'    rest of the CpGs
-#'
+#'    column names = sample ids. This should include the CpGs in \code{CpGs_char},
+#'    as well as additional CpGs.
+#' @param rDropThresh_num thershold for min correlation between a cpg with sum
+#'    of the rest of the CpGs
 #' @param arrayType Type of array, can be "450k" or "EPIC"
-#'
-#' @param returnAllCpGs When there is not a contiguous comethylated region in the inputing
-#' pre-defined region, \code{returnAllCpGs = 1} indicates outputting all the CpGs in the input
-#' region, \code{returnAllCpGs = 0} indicates not returning any CpG.
+#' @param returnAllCpGs When there is not a contiguous comethylated region in
+#'    the inputing pre-defined region, \code{returnAllCpGs = 1} indicates
+#'    outputting all the CpGs in the input region, while \code{returnAllCpGs = 0}
+#'    indicates not returning any CpG.
 #'
 #' @return A list with two components:
-#'
-#' \itemize{
-#'   \item{\code{Contiguous_Regions} : }{a data frame with \code{CpG} (CpG ID),
-#'    \code{Chr} (chromosome number), \code{MAPINFO} (genomic position), \code{r_drop} (correlation between the CpG with rest of the CpGs), \code{keep} (indicator for co-methylated CpG),
-#'    \code{keep_contiguous} (index for contiguous comethylated subregion)}
-#'
-#'   \item{\code{CpGs_subregions} : }{lists of CpGs in each contiguous co-methylated
-#'       subregion}
-#'
-#' }
+#'   \itemize{
+#'     \item{\code{Contiguous_Regions} : }{a data frame with \code{CpG} (CpG ID),
+#'       \code{Chr} (chromosome number), \code{MAPINFO} (genomic position),
+#'       \code{r_drop} (correlation between the CpG with rest of the CpGs),
+#'       \code{keep} (indicator for co-methylated CpG), \code{keep_contiguous}
+#'       (index for contiguous comethylated subregion)
+#'     }
+#'     \item{\code{CpGs_subregions} : }{lists of CpGs in each contiguous
+#'       co-methylated subregion
+#'     }
+#'   }
 #'
 #' @export
 #'
 #' @examples
 #'    data(betaMatrixChr22_df)
-#'    CpGsChr22_char <- c("cg02953382", "cg12419862", "cg24565820", "cg04234412",
-#'       "cg04824771", "cg09033563", "cg10150615", "cg18538332", "cg20007245",
-#'       "cg23131131", "cg25703541")
-#'    CoMethSingleRegion (CpGs_char = CpGsChr22_char,
-#'       betaMatrix = betaMatrixChr22_df)
+#'    CpGsChr22_char <- c(
+#'      "cg02953382", "cg12419862", "cg24565820", "cg04234412", "cg04824771",
+#'      "cg09033563", "cg10150615", "cg18538332", "cg20007245", "cg23131131",
+#'      "cg25703541"
+#'    )
+#'    CoMethSingleRegion(
+#'      CpGs_char = CpGsChr22_char,
+#'      betaMatrix = betaMatrixChr22_df
+#'    )
 #'
 #'    data(betaCluster_mat_example3)
 #'    betaMatrix_ex3 <- t(betaMatrix_ex3)
-#'    CpGsEx3_char <- c("cg14221598", "cg02433884", "cg07372974", "cg13419809",
-#'       "cg26856676", "cg25246745")
-#'    CoMethSingleRegion (CpGs_char = CpGsEx3_char, betaMatrix = betaMatrix_ex3,
-#'       returnAllCpGs = TRUE)
+#'    CpGsEx3_char <- c(
+#'      "cg14221598", "cg02433884", "cg07372974", "cg13419809", "cg26856676",
+#'      "cg25246745"
+#'    )
+#'    CoMethSingleRegion(
+#'      CpGs_char = CpGsEx3_char,
+#'      betaMatrix = betaMatrix_ex3,
+#'      returnAllCpGs = TRUE
+#'    )
 #'
 CoMethSingleRegion <- function(CpGs_char, betaMatrix, rDropThresh_num = 0.5,
                                arrayType = c("450k","EPIC"),
@@ -64,8 +70,10 @@ CoMethSingleRegion <- function(CpGs_char, betaMatrix, rDropThresh_num = 0.5,
   betaClusterTransp_mat <- t(betaCluster_mat)
 
   ### Mark comethylated CpGs ###
-  keepCpGs_df <- MarkComethylatedCpGs(betaCluster_mat = betaClusterTransp_mat,
-                                      rDropThresh_num)
+  keepCpGs_df <- MarkComethylatedCpGs(
+    betaCluster_mat = betaClusterTransp_mat,
+    rDropThresh_num
+  )
 
   ### Find contiguous comethylated regions ###
   keepContiguousCpGs_df <- FindComethylatedRegions(
@@ -87,7 +95,6 @@ CoMethSingleRegion <- function(CpGs_char, betaMatrix, rDropThresh_num = 0.5,
     contiguousRegions = coMethCpGs_df,
     CpGsSubregions = keepContiguousCpGs_ls
   )
-
 
   coMethCpGs_ls
 
