e93d06231e871c3432fc34432c1a24cec7a1dfaa
lilyAtGitlab <lily.wangg@gmail.com>
2019-03-27 10:55:37 -0400

    switch row and column descriptions in datset

--- R/data_betaMatrix_ex2.R
+++ R/data_betaMatrix_ex2.R
@@ -3,8 +3,8 @@
 #' @description Subset of an Alzheimer's Disease methylation data set,
 #'   with beta values for CpGs.
 #'
-#' @format A data frame containing beta values for 4 CpGs in one CpG islands for 110 subjects. Each column is a sample,
-#' each row is a CpG.
+#' @format A data frame containing beta values for 4 CpGs in one CpG islands for 110 subjects.
+#' Each column is a CpG, each row is a sample.
 #'
 #' @source GEO accession: GSE59685
 "betaMatrix_ex2"
