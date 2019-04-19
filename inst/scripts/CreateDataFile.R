7ecf683c566fae33480bdaee43e71a14bd5181d3
Gabriel J. Odom <gjo15@med.miami.edu>
2019-04-19 17:35:12 +0000

    formatting changes

--- inst/scripts/CreateDataFile.R
+++ inst/scripts/CreateDataFile.R
@@ -1,5 +1,5 @@
 # Download and Clean GEO Data for Example
-# Lissette Gomez and Lizhong Liu
+# Lissette Gomez, Lizhong Liu, and Lily Wang
 # 2019-04-15
 # Last edited: 2019-04-19
 
@@ -22,24 +22,32 @@ getGEOSuppFiles(
 )
 
 betas <- fread(
-  system.file ("extdata/GSE59685",
-               "GSE59685_betas.csv.gz",
-               package = 'coMethDMR',
-               mustWork = TRUE)
+  system.file(
+    "extdata/GSE59685",
+    "GSE59685_betas.csv.gz",
+    package = 'coMethDMR',
+    mustWork = TRUE
+  )
 )
 
 
 ### Get pheno data ###
 pheno <- test$GSE59685_series_matrix.txt.gz@phenoData@data
 
-pheno$stage <- as.numeric(substring(pheno$characteristics_ch1.3, 13,14))
+# Add comments to describe *why* you are doing all this.
+pheno$stage      <- as.numeric(substring(pheno$characteristics_ch1.3, 13, 14))
 pheno$subject.id <- as.numeric(substr(pheno$characteristics_ch1, 11, 14))
-pheno$Mplate <- as.factor(substr(pheno$characteristics_ch1.1, 9,19))
-pheno$sex <- as.factor (pheno$characteristics_ch1.4)
-pheno$sample.id <- rownames(pheno)
-pheno$age.brain <- as.numeric(substr(pheno$characteristics_ch1.6, 11, 14))
-pheno$age.blood <- as.numeric(substr(pheno$characteristics_ch1.5, 11, 14))
-pheno_df <- pheno[pheno$source_name_ch1 == "frontal cortex" ,
-                   c("stage",  "subject.id", "Mplate", "sex", "sample.id", "age.brain")]
-pheno_df <- pheno_df[!is.na(pheno_df$stage),]
+pheno$Mplate     <- as.factor(substr(pheno$characteristics_ch1.1, 9, 19))
+pheno$sex        <- as.factor(pheno$characteristics_ch1.4)
+pheno$sample.id  <- rownames(pheno)
+pheno$age.brain  <- as.numeric(substr(pheno$characteristics_ch1.6, 11, 14))
+pheno$age.blood  <- as.numeric(substr(pheno$characteristics_ch1.5, 11, 14))
+
+pheno_df         <- pheno[
+  pheno$source_name_ch1 == "frontal cortex" ,
+  c("stage",  "subject.id", "Mplate", "sex", "sample.id", "age.brain")
+]
+pheno_df         <- pheno_df[!is.na(pheno_df$stage), ]
+
+
 saveRDS(pheno_df, "pheno_df.rds")
