ae58e90be1ec433384d9674643f8f6c09466e811
lissettegomez <lxg255@miami.edu>
2019-08-14 17:20:47 -0400

    minor edits, issue#50

--- inst/scripts/convert__rdsFile_to_gmtFiles.R
+++ inst/scripts/convert__rdsFile_to_gmtFiles.R
@@ -1,7 +1,9 @@
 
 library(pathwayPCA)
 
-genomic_region<-c("ISLAND_3_200","NSHELF_3_200","SSHELF_3_200","NSHORE_3_200","SSHORE_3_200","TSS200_3_200","TSS1500_3_200","GENEBODY_3_200","UTR3_3_200","UTR5_3_200","EXON1_3_200")
+genomic_region <- c("ISLAND_3_200", "NSHELF_3_200", "SSHELF_3_200", "NSHORE_3_200",
+                  "SSHORE_3_200", "TSS200_3_200", "TSS1500_3_200",
+                  "GENEBODY_3_200", "UTR3_3_200", "UTR5_3_200", "EXON1_3_200")
 
 for (g in genomic_region[1:length(genomic_region)]){
   region_file <- readRDS(paste0(g,".RDS"))
