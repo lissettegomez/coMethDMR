70f6e946867fbb58dbcd268d9b7af932639db940
lissettegomez <lxg255@miami.edu>
2019-08-20 11:00:29 -0400

    adding script for creating the chr22 example file, issue#50

---
+++ inst/scripts/create_chr22exFile.R
@@ -0,0 +1,11 @@
+
+## https://github.com/rforge/ima/blob/master/www/fullannotInd.rda
+load("fullannotInd.rda")
+annot <- as.data.frame(fullannot)
+
+islands <- readRDS("ISLAND_3_200.rds")
+
+islandName <- names(islands)
+islandNameChr22 <- islandName[grep("chr22:", islandName)][1:20]
+islandsChr22 <- islands[islandNameChr22]
+saveRDS(islandsChr22, "CpGislandsChr22_ex.RDS")
