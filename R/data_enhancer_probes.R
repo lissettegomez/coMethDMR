ebce0fa1b05de637ae79c563364578b9e3604b22
Fernanda Veitzman <fveit001@fiu.edu>
2021-03-19 14:07:00 -0400

    Added data documentation.

---
+++ R/data_enhancer_probes.R
@@ -0,0 +1,8 @@
+#' Example of genic regions that contains the CpGs.
+#'
+#' @description Genic regions that contains the CpGs.
+#'
+#' @format A large character vector containing 102559 elements, 7.4 MB.
+#'
+#' @source
+"enhancer.pheno"
